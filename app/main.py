9c8e35398b0e7056778bd95666dfbb1409db2c40
bahar toghir <bahar.toghir@gmail.com>
2025-12-25 23:52:47 +0330

    feat: add files

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+store = {}
+
+@app.post("/set")
+def set_value(key: str, value: str):
+    store[key] = value
+    return {"message": "value stored successfully"}
+
+@app.get("/get")
+def get_value(key: str):
+    if key not in store:
+        raise HTTPException(status_code=404, detail="key not found")
+    return {"key": key, "value": store[key]}
